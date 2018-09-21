8d2cb7beccbac991f1111fbcb20e26ceef485026
kevinshin <tshin@macalester.edu>
2018-09-20 23:03:56 -0500

    got all the math functions working

--- test_Godel.py
+++ test_Godel.py
@@ -1,3 +1,5 @@
+import math
+
 symbolDict = {'0':1,'s':2,'+':3,'*':4,'=':5,'(':6,')':7,',':8,'x':9,'|':10,'~':11,'&':12,'∃':13}
 '''differs from Crossley symbols because user needs to be able to type in'''
 
@@ -19,30 +21,53 @@ def generatePrimes(number):
     start = number-2
     primes = [2,3]
     starter = 5
-    while start != 0:
-        if isitprime(starter):
-            primes.append(starter)
-            starter += 1
-            start -= 1
-        else:
-            starter += 1
+    if number == 1:
+        return [2]
+    elif number == 2:
+        return primes
+    else:
+        while start != 0:
+            if isitprime(starter):
+                primes.append(starter)
+                starter += 1
+                start -= 1
+            else:
+                starter += 1
     return primes
 
-def primeFactorization(n):
-    if isitprime(n):
-        return [1,n]
-    else:
-        a = 2
-        factors = []
-        while a <= n/2:
-            if n%a == 0:
-                b = n/a
-                factors.append(a)
-                factors.append(b)
-                a+=1
-            else:
-                a += 1
-        return factors
+
+def convertToGodelFactors(formula):
+    '''takes in formula as a string, prints prime encoding and Godel Number'''
+    length = len(formula)
+    primes = generatePrimes(length)
+    i = 0
+    godelFormula = ''
+    for char in formula:
+        godelFormula = godelFormula + str(primes[i]) + "**" + str(symbolDict[char])+'.'
+        i +=1
+    return godelFormula[:-1]
+
+def converttoGodelNumber(formula):
+    length = len(formula)
+    primes = generatePrimes(length)
+    i = 0
+    number = 1
+    for char in formula:
+        number = number * primes[i]**symbolDict[char]
+        i +=1
+    return number
 
 
-print(primeFactorization(15))
+def getPrimeFactorization(number):
+    a = 2
+    factors = []
+    while a <= math.ceil(math.sqrt(number)):
+        if number%a != 0:
+            a += 1
+        else:
+            number = number/a
+            factors.append(a)
+    if number != 1:
+        factors.append(number)
+    return factors
+
