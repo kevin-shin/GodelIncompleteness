82278933a2d9ff74a34e2224d676adf3ef932ef4
kevinshin <tshin@macalester.edu>
2019-01-15 15:00:43 -0600

    Getting value errors, and not sure why, but parsing runs. Read value part of PLY a little more carefully.

--- parser.py
+++ parser.py
@@ -6,20 +6,55 @@ tokens = ('ZERO', 'SUCCESSOR', 'PLUS', 'TIMES', 'EQUALS',
           'EXISTS', 'END')
 
 #TOKENS
-t_ZERO = r'0'
-t_SUCCESSOR = r's'
-t_PLUS = r'\+'
-t_TIMES = r'\*'
-t_EQUALS = r'='
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_NEXT = r'\,'
-t_VARIABLE = r'x'
-t_NOT = r'~'
-t_AND = r'\&'
-t_EXISTS = r'E'
-t_END = r'\;'
 
+#t_ZERO = r'0'
+#t_SUCCESSOR = r's'
+#t_PLUS = r'\+'
+#t_TIMES = r'\*'
+#t_EQUALS = r'='
+#t_LPAREN = r'\('
+#t_RPAREN = r'\)'
+#t_NEXT = r'\,'
+#t_VARIABLE = r'x'
+#t_NOT = r'~'
+#t_AND = r'\&'
+#t_EXISTS = r'E'
+#t_END = r'\;'
+
+def t_ZERO(t):
+    r'0'
+    t.value = int(t.value)
+    return t
+
+def t_SUCCESSOR(t):
+    r's'
+    t.value = "s"
+    return t
+
+def t_PLUS(t):
+    r'\+'
+    t.value = "+"
+    return t
+
+def t_TIMES(t):
+    r'\*'
+    t.value = "*"
+    return t
+
+def t_EQUALS(t):
+    r'='
+    t.value = "="
+    return t
+
+def t_LPAREN(t):
+    r'\('
+    t.value = "("
+    return t
+
+def t_RPAREN(t):
+    r'\)'
+    t.value = ")"
+    return t
 
 def t_newline(t):
     r'\n+'
@@ -38,43 +73,53 @@ precedence = (
     ('left', 'TIMES'),
 )
 
+
 def p_start(p):
     '''S : L
         | L S'''
-    print(p[0] + "Start Variable")
-
+    print("Start Variable recognized")
 
 def p_l(p):
     '''L : END
         | EXPRESSIONS END
         | STATEMENTS END'''
+    print("L Recognized")
 
 def p_statement(p):
     '''STATEMENTS : LPAREN EXPRESSIONS AND EXPRESSIONS RPAREN
                  | EXISTS VARIABLE LPAREN EXPRESSIONS LPAREN
                  | NOT EXPRESSIONS'''
+    print("Statement Recognized")
 
 def p_expression(p):
     '''EXPRESSIONS : E1 EQUALS E1'''
+    print("Expression recognized")
 
 def p_e1(p):
     '''E1 : PLUS LPAREN E1 NEXT E1 RPAREN
           | E2'''
+    print("E1 recognized")
 
 def p_e2(p):
     '''E2 : TIMES LPAREN E2 NEXT E2 RPAREN
          | E3'''
+    print("E2 recognized")
 
 def p_e3(p):
     '''E3 : int
          | VARIABLE'''
+    print("E3 recognized")
 
 def p_int(p):
     '''int : ZERO
           | SUCCESSOR LPAREN int RPAREN'''
+    print("Integer Recognized")
+    p[0] = p[3]+p[3]
 
 def p_error(p):
+    print("Error, not a well formed sentence of the language.")
     print("Syntax error at '%s'" % p.value)
+
 
 yacc.yacc()
 
