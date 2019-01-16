b3a3aa2491ea53c7fb997841a842708d98ff0472
kevinshin <tshin@macalester.edu>
2019-01-16 11:46:26 -0600

    Wait - it works...

--- parser.py
+++ parser.py
@@ -1,59 +1,34 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
-tokens = ('ZERO', 'SUCCESSOR', 'PLUS', 'TIMES', 'EQUALS',
+tokens = ['ZERO', 'SUCCESSOR', 'PLUS', 'TIMES', 'EQUALS',
           'LPAREN', 'RPAREN', 'NEXT', 'VARIABLE', 'NOT', 'AND',
-          'EXISTS', 'END')
+          'EXISTS', 'END']
 
 #TOKENS
 
 #t_ZERO = r'0'
 #t_SUCCESSOR = r's'
-#t_PLUS = r'\+'
-#t_TIMES = r'\*'
-#t_EQUALS = r'='
-#t_LPAREN = r'\('
-#t_RPAREN = r'\)'
-#t_NEXT = r'\,'
-#t_VARIABLE = r'x'
-#t_NOT = r'~'
-#t_AND = r'\&'
-#t_EXISTS = r'E'
-#t_END = r'\;'
+t_PLUS = r'\+'
+t_TIMES = r'\*'
+t_EQUALS = r'='
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NEXT = r'\,'
+t_VARIABLE = r'x'
+t_NOT = r'~'
+t_AND = r'\&'
+t_EXISTS = r'E'
+t_END = r'\;'
 
 def t_ZERO(t):
     r'0'
-    t.value = int(t.value)
+    t.value = 0
     return t
 
 def t_SUCCESSOR(t):
     r's'
     t.value = "s"
-    return t
-
-def t_PLUS(t):
-    r'\+'
-    t.value = "+"
-    return t
-
-def t_TIMES(t):
-    r'\*'
-    t.value = "*"
-    return t
-
-def t_EQUALS(t):
-    r'='
-    t.value = "="
-    return t
-
-def t_LPAREN(t):
-    r'\('
-    t.value = "("
-    return t
-
-def t_RPAREN(t):
-    r'\)'
-    t.value = ")"
     return t
 
 def t_newline(t):
@@ -77,55 +52,56 @@ precedence = (
 def p_start(p):
     '''S : L
         | L S'''
-    print("Start Variable recognized")
+    p[0] = None
 
 def p_l(p):
-    '''L : END
-        | EXPRESSIONS END
-        | STATEMENTS END'''
-    print("L Recognized")
+    '''L : EXPRESSIONS END
+         | STATEMENTS END
+         | END '''
+    p[0] = None
 
 def p_statement(p):
     '''STATEMENTS : LPAREN EXPRESSIONS AND EXPRESSIONS RPAREN
                  | EXISTS VARIABLE LPAREN EXPRESSIONS LPAREN
                  | NOT EXPRESSIONS'''
-    print("Statement Recognized")
+    p[0] = None
 
 def p_expression(p):
-    '''EXPRESSIONS : E1 EQUALS E1'''
-    print("Expression recognized")
+    '''EXPRESSIONS : E1
+                   | E1 EQUALS E1'''
+    p[0] = None
 
 def p_e1(p):
-    '''E1 : PLUS LPAREN E1 NEXT E1 RPAREN
-          | E2'''
-    print("E1 recognized")
+    '''E1 : E2
+          | PLUS LPAREN E1 NEXT E1 RPAREN'''
+    print("Plus Recognized")
+    p[0] = None
 
 def p_e2(p):
-    '''E2 : TIMES LPAREN E2 NEXT E2 RPAREN
-         | E3'''
-    print("E2 recognized")
+    '''E2 : E3
+          | TIMES LPAREN E2 NEXT E2 RPAREN '''
+    p[0] = None
 
 def p_e3(p):
     '''E3 : int
-         | VARIABLE'''
-    print("E3 recognized")
+          | VARIABLE'''
+    p[0] = None
 
 def p_int(p):
     '''int : ZERO
-          | SUCCESSOR LPAREN int RPAREN'''
-    print("Integer Recognized")
-    p[0] = p[3]+p[3]
+           | SUCCESSOR LPAREN int RPAREN'''
+    p[0] = None
 
 def p_error(p):
     print("Error, not a well formed sentence of the language.")
-    print("Syntax error at '%s'" % p.value)
+
 
 
 yacc.yacc()
 
 while True:
     try:
-        s = input('calc > ')   # use input() on Python 3
+        s = input('>>> ')   # use input() on Python 3
     except EOFError:
         break
     yacc.parse(s)
