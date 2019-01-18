7dd94bdc30ac88d2696fff0a1ba61fde53418b93
kevinshin <tshin@macalester.edu>
2019-01-18 11:19:46 -0600

    Main intro done. Onto docstrings and comments.

--- src/GodelIncompleteness.py
+++ src/GodelIncompleteness.py
@@ -1,3 +1,47 @@
+#######################################################################################################################
+#                                                                                                                     #
+#                                          Godel Incompleteness by Kevin Shin                                         #
+#                                                                                                                     #
+#######################################################################################################################
+
+'''
+This project was initiated after taking PHIL 313, Advanced Symbolic Logic at Macalester College, in which I was exposed
+to the full proof of Godel's Incompleteness Theorem, which states that any system that uses arithmetic is incomplete -
+there are sentences which are unprovable (and true). The basic idea behind the proof is to use numbers to encode formulas
+of logic, and then to specify a sentence (the Godel sentence) which cannot be proven.
+
+In most versions of the proof, the details for the numbering and encoding are left to be an ambiguous portion
+of the proof, presumably because from a theoretical perspective, the details of the encoding are unimportant
+as long as it is doable and sound. However, the actual encoding procedure assembles an interesting
+melange of skills from number theory, language theory, and presents a unique challenge to the coder to devise
+an algorithm capable of handling arbitrary sentences of arithmetic.
+
+Thus, the intention of this project is simply to provide a GUI in which the user is able to type in sentences of
+arithmetic and have the program output the Godel number of the sentence, if it is well-formed by the syntax and
+symbolization as provided by Crossley in "What is Mathematical Logic?".
+
+Another important intention behind this project was to provide this interface as a teaching tool, especially in light of
+the complex and abstract nature of the proof. Although Godel numbering is explained as a necessary concept but then
+bypassed, students are encouraged to remember that the entire proof hinges upon the ability to use such a numbering
+to use arithmetic to talk about arithmetic, which leads to the Godel sentence. Thus, although semantically a recursive
+process, the importance of this portion of the proof cannot be understated. The idea is that if this process can be
+automated and presented in an intuitive way, students will be able to understand the theoretical moves Godel makes
+more clearly.
+
+Although the repository contains a diverse range of directories and files, the basic organization of this project is
+the following:
+    - GodelIncompleteness.py contains a GUI tkinter window which gives the user access to all the functionality
+      aforementioned. This is the file which will actually be run.
+    - parser.py is the parser developed for the grammar describing arithmetic using Crossley's symbols and rules.
+      Interested readers are invited to read this file to see the tokens, lexer, and parsers in play.
+    - mathHelperFunctions.py contains most of the functions used for the number theory portion of this project, largely
+      involving the generation of prime numbers and prime factorizations of numbers.
+
+For a full description of the project as well as credits and copyrights, please refer to the README in the associated
+github repository at: https://github.com/kevin-shin/GodelIncompleteness.
+
+'''
+
 import tkinter as tk
 import webbrowser
 from tkinter import *
